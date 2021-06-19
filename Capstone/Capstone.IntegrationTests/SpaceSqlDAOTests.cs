b21dfe35db4594d247c867c7966e572dde993c99
Malick Sow <malicksow807@gmail.com>
2021-06-19 18:00:49 -0400

    EOD commit

--- Capstone/Capstone.IntegrationTests/SpaceSqlDAOTests.cs
+++ Capstone/Capstone.IntegrationTests/SpaceSqlDAOTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Capstone.DAL;
+using Capstone.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,18 @@ namespace Capstone.IntegrationTests
 {
    public class SpaceSqlDAOTests: IntegrationTestBase
     {
+        public void GetSpaceInfoTest_Should_ReturnAllSpaces()
+        {
+            // Arrange
+            SpaceSqlDAO dao = new SpaceSqlDAO(this.ConnectionString);
+            int expectedResults = this.GetRowCount("space");
+
+            // Act
+            IList<Space> results = dao.GetSpaceInfo("name");
+
+            // Assert
+            Assert.IsTrue(results.Count > 0);
+            Assert.AreEqual(expectedResults, results.Count);
+        }
     }
 }
