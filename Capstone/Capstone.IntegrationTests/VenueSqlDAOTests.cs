53f020ba113d11bc809e17af7b6b165a0bdbe32e
Malick Sow <malicksow807@gmail.com>
2021-06-19 12:22:43 -0400

    pair project tests

---
+++ Capstone/Capstone.IntegrationTests/VenueSqlDAOTests.cs
@@ -0,0 +1,28 @@
+﻿using Capstone.DAL;
+using Capstone.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.IntegrationTests
+{
+   public class VenueSqlDAOTests : IntegrationTestBase
+    {
+
+        [TestMethod]
+        public void GetVenuesTest_Should_ReturnAllVenues()
+        {
+            // Arrange
+            VenueSqlDAO dao = new VenueSqlDAO(this.ConnectionString);
+            int expectedResults = this.GetRowCount("venue");
+
+            // Act
+            IList<Venue> results = dao.ListVenues();
+
+            // Assert
+            Assert.IsTrue(results.Count > 0);
+            Assert.AreEqual(expectedResults, results.Count);
+        }
+    }
+}
